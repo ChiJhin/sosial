bd265226d9603cdc5f54b64321553a528bbd75dc
Dimitry Minchukov <jerek.d.lem@gmail.com>
2024-02-07 16:56:57 +0200

    contacts fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@ import 'modern-normalize';
 import '../sass/main.scss';
 
 
-const specialistList = document.querySelector('.header__specialists-list');
+const pageNav = document.querySelector('.header__page-nav');
+const pageNavList = document.querySelector('.header__page-nav-list');
 const toHome = document.getElementById('to-home');
 const menu = document.getElementById('menu-list')
 console.log(toHome);
@@ -11,10 +12,11 @@ console.log(menu);
 const title = document.title;
 
 if (!title.includes('Психологічна підтримка')) {
-  specialistList.style.display = 'none';
+  pageNav.style.display = 'none';
   toHome.style.display = 'block';
 } else {
-  specialistList.style.display = 'flex';
+  pageNav.style.display = 'blok';
+  pageNavList.style.display = 'flex';
   toHome.style.display = 'none';
 }
 
