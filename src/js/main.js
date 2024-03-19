45368e538807937b296108b0a82a1e0f361ec00b
Dimitry Minchukov <jerek.d.lem@gmail.com>
2024-03-20 01:04:59 +0200

    scroll fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import 'modern-normalize';
 import '../sass/main.scss';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 
 const pageNav = document.querySelector('.header__page-nav');
@@ -27,14 +28,17 @@ const closeMenuBtn = document.querySelector('.js-close-menu');
 
 const toggleMenu = () => {
   const isMenuOpen =
-    openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.getAttribute('aria-expanded') === true || false;
   openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
   mobileMenu.classList.toggle('is-open');
 
-  const scrollLockMethod = !isMenuOpen
-    ? 'disableBodyScroll'
-    : 'enableBodyScroll';
-  bodyScrollLock[scrollLockMethod](document.body);
+  if (!isMenuOpen) {
+    disableBodyScroll(mobileMenu)
+  }else{
+    enableBodyScroll(mobileMenu)
+
+  }
+  
 };
 
 openMenuBtn.addEventListener('click', toggleMenu);
@@ -45,5 +49,5 @@ window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
   if (!e.matches) return;
   mobileMenu.classList.remove('is-open');
   openMenuBtn.setAttribute('aria-expanded', false);
-  bodyScrollLock.enableBodyScroll(document.body);
+  enableBodyScroll(document.body);
 });
